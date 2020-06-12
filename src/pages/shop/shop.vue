81ffe5497e80a0822bcda67ffa6ac8f9a8522c62
xingyanhai <1101099728@qq.com>
2020-06-13 00:07:45 +0800

    加tabbar

---
+++ src/pages/shop/shop.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="content">
+		<view class="btn-row">
+			商城
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+		mapState,
+		mapMutations
+	} from 'vuex'
+
+	export default {
+		computed: {
+			...mapState(['hasLogin', 'forcedLogin'])
+		},
+		methods: {
+			...mapMutations(['logout']),
+		}
+	}
+</script>
+
+<style>
+
+</style>
